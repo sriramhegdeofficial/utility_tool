e2489b6e16a2739bc2f8bd442abc738fa3b7f7db
sriramhegde <sriramhegdeofficial@gmail.com>
2019-12-19 15:07:56 +0530

    added input for changing percent value of loader

--- src/App.js
+++ src/App.js
@@ -2,10 +2,36 @@ import React, {Component} from 'react';
 import {CircleProgress} from 'react-gradient-progress';
 
 
+
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            percent: 50
+            
+        };
+    }
+    handleChange = (event) => {
+       let  percentValue = parseInt(event.target.value);
+
+        if(percentValue >= 0 || percentValue <= 100){
+            this.setState({
+                percent: percentValue
+            })
+        }
+        
+    }
     render(){
+        {console.log(this.state.percent)}
         return(
-            <CircleProgress percentage={70} strokeWidth={15} secondaryColor="#f0f0f0" />
+            <div>
+                
+            <CircleProgress percentage={this.state.percent} strokeWidth={15} primaryColor= {["red", "green","green","yellow"]}  secondaryColor="#f0f0f0" />
+            <input 
+            
+            onChange = {this.handleChange}
+            />
+            </div>
         );
     }
 }
